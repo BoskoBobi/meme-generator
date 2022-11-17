2b318ca37475c8e545c8df0608dfd44625c60911
Boban <bosko_bobi@yahoo.com>
2022-11-17 04:23:09 +0100

    second commit

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,12 +2,19 @@
 
 export default function Meme () {
   return (
-    <section className="head">
-      <input className="head--input_text" type="text" />
-      <input className="head--input_text" type="text" />
-      <button className="head--button--generator">
-        Get a new meme image
-      </button>
-    </section>
+    <main className="meme">
+      <div className="form">
+        <div className="form--inputs--flex">
+          <input className="form--input_text" type="text" 
+          placeholder="Shut up" />
+          <input className="form--input_text" type="text" 
+          placeholder="and take my money" />
+        </div>
+        <button className="form--button--generator">
+          Get a new meme image 🖼
+        </button>
+      </div>
+      
+    </main>
   )
 }
